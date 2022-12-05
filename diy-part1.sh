628867f2c0a75f30f9de00154df210ba9bcbf015
hellcatjack <hellcatjack@gmail.com>
2022-12-05 14:11:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,13 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+cd package/lean
+svn checkout https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git
+cd ../..
+
+sed -i '/^exit 0/i \/data\/shell\/runonce.sh' package/base-files/files/etc/rc.local
